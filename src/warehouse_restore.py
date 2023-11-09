b8d4304c6d8b874b7bc834b5f24b2332b4f2f73a
Melody Tung <melodytung@Melodys-MacBook-Pro.local>
2023-11-09 20:22:51 +0100

    Local file of model in src

--- src/warehouse_restore.py
+++ src/warehouse_restore.py
@@ -25,6 +25,48 @@ best_model = wandb.restore('model_seasonal_extended.h5', run_path=f"{username}/{
 # Create the PPO model with the same configuration used during training
 model = PPO.load(best_model.name)
 
+# Create the PPO model with the same configuration used during training
+model = PPO.load(best_model.name)
+
+
+
+# import os
+#
+# # Save the model locally
+# local_model_filename = "local_model_seasonal_extended"
+# model.save(local_model_filename)
+#
+# # Check if the model file exists locally
+# if os.path.isfile(local_model_filename):
+#     print(f"Model '{local_model_filename}' found locally. You can proceed to load and use it.")
+# else:
+#     print(f"Model '{local_model_filename}' not found locally. Make sure the model is saved with the correct filename.")
+
+import os
+
+# Get the absolute path to the directory where you want to save the model
+model_save_dir = "/Users/melodytung/PycharmProjects/FAIRWork-RL-Inventory-Management/src"
+local_model_filename = "local_model_seasonal_extended"
+
+# Create the directory if it does not exist
+if not os.path.exists(model_save_dir):
+    os.makedirs(model_save_dir)
+
+# Save the model to the specified directory
+model.save(os.path.join(model_save_dir, local_model_filename))
+
+
+# # Get the absolute path to the model file
+# model_file_path = os.path.join(model_save_dir, local_model_filename)
+#
+# # Check if the model file exists
+# if os.path.isfile(model_file_path):
+#     # Load the model
+#     model = PPO.load(model_file_path)
+# else:
+#     # The model file does not exist
+#     print("The model file does not exist")
+
 
 all_data_sets = []
 for seed in range(51, 61):  # This loop will include seeds from 51 to 60
